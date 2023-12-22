59bd7164abda5d3884fa76faf55b0abc971edac6
sutharsan sparkz <62829664+sparkz-technology@users.noreply.github.com>
2023-12-22 21:56:37 +0530

    updated

---
+++ src/features/Editer/Editer.tsx
@@ -0,0 +1,9 @@
+const Editer: React.FC = () => {
+   return (
+      <div>
+         <h1>Editer</h1>
+      </div>
+   )
+}
+
+export default Editer
---
+++ src/features/output/Output.tsx
@@ -0,0 +1,9 @@
+const Output: React.FC = () => {
+   return (
+      <div>
+         <h1>Output</h1>
+      </div>
+   )
+}
+
+export default Output
